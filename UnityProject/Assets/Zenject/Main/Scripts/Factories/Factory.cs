d454d6a8cc9dea35a6712c75f2132e6ada8da452
svermeulen <sfvermeulen@gmail.com>
2014-05-10 15:24:41 -0300

    Changed to not initialize IInitializables automatically from factories.  Removed warnings about tick/init prioritse

--- UnityProject/Assets/Zenject/Main/Scripts/Factories/Factory.cs
+++ UnityProject/Assets/Zenject/Main/Scripts/Factories/Factory.cs
@@ -17,20 +17,7 @@ namespace ModestTree.Zenject
 
         public virtual TContract Create(params object[] constructorArgs)
         {
-            var obj = Instantiator.Instantiate<TConcrete>(_container, constructorArgs);
-
-            if (typeof(IInitializable).IsAssignableFrom(typeof(TConcrete)))
-            {
-                // Runtime created classes can implement IInitializable to get an Initialize() method called
-                // directly after creation
-                // It's often undesirable to put too much logic in the constructor
-                // (http://blog.vuscode.com/malovicn/archive/2009/10/16/inversion-of-control-single-responsibility-principle-and-nikola-s-laws-of-dependency-injection.aspx)
-                // (http://blog.ploeh.dk/2011/03/03/InjectionConstructorsshouldbesimple/)
-                // so this is usually a good alternative
-                ((IInitializable)obj).Initialize();
-            }
-
-            return obj;
+            return Instantiator.Instantiate<TConcrete>(_container, constructorArgs);
         }
     }
 
@@ -57,15 +44,7 @@ namespace ModestTree.Zenject
 
         public virtual TContract Create(params object[] constructorArgs)
         {
-            var obj = (TContract)Instantiator.Instantiate(_container, _concreteType, constructorArgs);
-
-            if (typeof(IInitializable).IsAssignableFrom(_concreteType))
-            {
-                // See comment above
-                ((IInitializable)obj).Initialize();
-            }
-
-            return obj;
+            return (TContract)Instantiator.Instantiate(_container, _concreteType, constructorArgs);
         }
     }
 }
