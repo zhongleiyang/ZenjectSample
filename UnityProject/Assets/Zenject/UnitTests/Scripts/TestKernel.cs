e8515bc29741030a8d2021ea5af4c158b89183f0
svermeulen <sfvermeulen@gmail.com>
2014-05-13 01:55:39 -0300

    Removed unnecessary using statement

--- UnityProject/Assets/Zenject/UnitTests/Scripts/TestKernel.cs
+++ UnityProject/Assets/Zenject/UnitTests/Scripts/TestKernel.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Xml.Linq;
 using NUnit.Framework;
 using UnityEngine;
 using TestAssert = NUnit.Framework.Assert;
